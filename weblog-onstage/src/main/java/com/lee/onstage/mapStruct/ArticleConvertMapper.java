bbda999abad5b549739c538177d382c2316eb5d6
Acxfoxer <sunmonkeywudi@gmail.com>
2024-04-11 14:45:12 +0800

    添加文章实体转换类

--- weblog-onstage/src/main/java/com/lee/onstage/mapStruct/ArticleConvertMapper.java
+++ weblog-onstage/src/main/java/com/lee/onstage/mapStruct/ArticleConvertMapper.java
@@ -1,11 +1,11 @@
-package com.lee.onstage.mapperStruct;
+package com.lee.onstage.mapStruct;
 
 import com.lee.onstage.entity.Article;
 import com.lee.onstage.model.vo.ArticleVO;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(componentModel = "spring")
+@Mapper
 public interface ArticleConvertMapper {
     ArticleConvertMapper INSTANCE=Mappers.getMapper(ArticleConvertMapper.class);
     ArticleVO toArticleVO(Article article);
