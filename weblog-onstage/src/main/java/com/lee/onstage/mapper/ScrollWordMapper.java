e0cfe74d629ac771f8e4014e69e18763e9113a01
Acxfoxer <sunmonkeywudi@gmail.com>
2024-04-11 14:43:23 +0800

    解决@Mapper注解错误问题

--- weblog-onstage/src/main/java/com/lee/onstage/mapper/ScrollWordMapper.java
+++ weblog-onstage/src/main/java/com/lee/onstage/mapper/ScrollWordMapper.java
@@ -2,12 +2,12 @@ package com.lee.onstage.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.lee.onstage.entity.ScrollWorld;
-import org.mapstruct.Mapper;
+import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface ScrollWordMapper extends BaseMapper<ScrollWorld> {
 
-    public ScrollWorld getRandomOutPut();
+    ScrollWorld getRandomOutPut();
 
     void addOutPut(ScrollWorld output);
 }
