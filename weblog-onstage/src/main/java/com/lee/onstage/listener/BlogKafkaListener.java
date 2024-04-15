cf35f70d4c88765c0052e5b8c0adfa84bf349cdf
Lee <handsomeleesouth@qq.com>
2024-04-15 20:05:18 +0800

    解决kafka监听器,ackMode为manual_immediate是,offset偏移量设置问题

--- weblog-onstage/src/main/java/com/lee/onstage/listener/BlogKafkaListener.java
+++ weblog-onstage/src/main/java/com/lee/onstage/listener/BlogKafkaListener.java
@@ -3,9 +3,12 @@ package com.lee.onstage.listener;
 import com.lee.onstage.constants.KafkaConstants;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
 
 /**
  * kafka 监听器
@@ -13,11 +16,18 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j
 public class BlogKafkaListener {
+    @Resource
+    KafkaTemplate<String,Object> kafkaTemplate;
 
+    /**
+     * email主题消费者,注意设置的ackMode为MANUAL_IMMEDIATE是需引入Acknowledgment参数,调用acknowledge()手动提交偏移量
+     * @param msg
+     * @param acknowledgment
+     */
     @KafkaListener(topics = KafkaConstants.EMAIL,containerFactory = "kafkaListenerContainerFactory",id = "blog")
-    public void processMsg(ConsumerRecord<?,?> msg ){
+    public void processMsg(ConsumerRecord<?,?> msg, Acknowledgment acknowledgment){
         log.info("监听器监听到消息,offset:{},partition:{},key:{},value{}",
                 msg.offset(),msg.partition(),msg.key(),msg.value());
-
+        acknowledgment.acknowledge();
     }
 }
